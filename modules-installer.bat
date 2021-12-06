5cf0cd79edcb21692dd120f6701bdeb38daf52fa
kandikits <95465112+kandikits@users.noreply.github.com>
2021-12-06 10:31:42 +0530

    Update modules-installer.bat

--- modules-installer.bat
+++ modules-installer.bat
@@ -1,3 +1,3 @@
-curl -o requirements.txt https://raw.githubusercontent.com/balaji-munusamy/faq-virtual-agent/main/requirements.txt
+curl -o requirements.txt https://raw.githubusercontent.com/kandikits/faq-virtual-agent/main/requirements.txt
 pip install -r requirements.txt
 pause
